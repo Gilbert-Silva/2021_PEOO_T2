e270355d09bb2bd5587e8a4d1d21c24372348bf8
GilbertAzevedo <gilbertif1995@outlook.com>
2022-02-03 21:05:27 +0000

    Avaliação 02

---
+++ Av02R/q01.cs
@@ -0,0 +1,48 @@
+using System;
+
+class Program {
+  public static void Main() {
+    Console.Write("Informe o número de contas: ");
+    int n = int.Parse(Console.ReadLine());
+    Agua[] vetor = new Agua[n];
+    for (int i = 0; i < n; i++) {
+      Console.Write("Informe o mês/ano: ");
+      string ma = Console.ReadLine();
+      Console.Write("Informe o consumo em m3: ");
+      int c = int.Parse(Console.ReadLine());
+      Console.Write("Informe o valor em R$: ");
+      double v = double.Parse(Console.ReadLine());
+      Agua a = new Agua(ma, c, v);
+      vetor[i] = a;
+    }
+    int totalc = 0;
+    double totalg = 0;
+    foreach(Agua a in vetor) {
+      totalc = totalc + a.GetConsumo();
+      totalg = totalg + a.GetValor();
+    }
+    Console.WriteLine($"Consumo total em m3 - {totalc}");
+    Console.WriteLine($"Total gasto em R$ - {totalg:0.00}");
+  }
+}
+
+class Agua {
+  private string mesAno;
+  private int consumo;
+  private double valor;
+  public Agua(string ma, int c, double v) {
+    this.mesAno = ma;
+    this.consumo = c;
+    this.valor = v;
+  }
+  public string GetMesAno() {
+    return mesAno;
+  }
+  public int GetConsumo() {
+    return consumo;
+  }
+  public double GetValor() {
+    return valor;
+  }
+}
+
---
+++ Av02R/q02.cs
@@ -0,0 +1,63 @@
+using System;
+
+class Program {
+  public static void Main() {
+    Pessoa p1 = new Pessoa("P1", "E1", true);
+    Pessoa p2 = new Pessoa("P2", "E2", false);
+    Pessoa p3 = new Pessoa("P3", "E3", true);
+    //Console.WriteLine(p1);
+    //Console.WriteLine(p2);
+    //Console.WriteLine(p3);
+    Equipe e = new Equipe("Equipe");
+    e.Inserir(p1);
+    e.Inserir(p2);
+    e.Inserir(p3);
+    foreach(Pessoa p in e.Listar())
+      Console.WriteLine(p);
+    Console.WriteLine(e.Lider());  
+  }
+}
+
+class Equipe {
+  private string nome;
+  private Pessoa[] vetor = new Pessoa[10];
+  private int k;
+  public Equipe(string n) {
+    this.nome = n;
+  }
+  public void Inserir(Pessoa p) {
+    if (Lider() == null || p.GetLider()==false)
+      if (k < 10) {
+        vetor[k] = p;
+        k++;
+      }
+  }
+  public Pessoa[] Listar() {
+    Pessoa[] aux = new Pessoa[k];
+    Array.Copy(vetor, aux, k);
+    return aux;
+  }
+  public Pessoa Lider() {
+    foreach(Pessoa p in vetor) {
+      if (p != null && p.GetLider()) return p;
+    }
+    return null;
+  }
+}
+
+class Pessoa {
+  private string nome;
+  private string email;
+  private bool lider;
+  public Pessoa(string n, string e, bool l) {
+    this.nome = n;
+    this.email = e;
+    this.lider = l;
+  }
+  public bool GetLider() {
+    return lider;
+  }
+  public override string ToString() {
+    return $"{nome} - {email} - {lider}";
+  }
+}
